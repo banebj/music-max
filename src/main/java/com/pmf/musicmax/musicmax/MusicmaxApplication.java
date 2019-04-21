a7bb87875236cd2858f2680bedfc24e3eee14a86
BANE_ACER\baneb <banebj@gmail.com>
2019-04-21 22:44:29 +0200

    initial commit

---
+++ src/main/java/com/pmf/musicmax/musicmax/MusicmaxApplication.java
@@ -0,0 +1,13 @@
+package com.pmf.musicmax.musicmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicmaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmf/musicmax/musicmax/MusicmaxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pmf.musicmax.musicmax;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicmaxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
