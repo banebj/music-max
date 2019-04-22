3bc2b83a39f2d0a4a38eee02f9ac37f44bd37a34
Branislav Jovanovic <branislav.jovanovic@devoteam.com>
2019-04-22 13:00:26 +0200

    TK-2199 kreiranje scheme i konekcije nad bazm

--- src/main/java/com/pmf/musicmax/MusicmaxApplication.java
+++ src/main/java/com/pmf/musicmax/MusicmaxApplication.java
@@ -1,4 +1,4 @@
-package com.pmf.musicmax.musicmax;
+package com.pmf.musicmax;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
