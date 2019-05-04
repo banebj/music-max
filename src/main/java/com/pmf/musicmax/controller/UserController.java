909ac3894a4caaee1563c7ac69b4e22657098b00
DusanStanuc <dusan.stanic97@htomail.com>
2019-05-04 10:00:27 +0200

    Update UserController

--- src/main/java/com/pmf/musicmax/controller/UserController.java
+++ src/main/java/com/pmf/musicmax/controller/UserController.java
@@ -1,26 +1,33 @@
 package com.pmf.musicmax.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.pmf.musicmax.model.User;
+import com.pmf.musicmax.repository.UserRepo;
 import com.pmf.musicmax.repository.UserRepository;
 
-@RestController
-@RequestMapping(value="/users")
+
+@Controller
+@RequestMapping(value="user")
 public class UserController {
 
 	@Autowired
 	UserRepository ur;
 	
+	@Autowired
+	UserRepo urr;
+	
 	@RequestMapping(value="/save", method=RequestMethod.POST)
 	public String saveUser(User u, Model m) {
-		ur.save(u);
-		m.addAttribute("message", "Korisnik je uspesno sacuvan.");
+		/*ur.save(u);
+		m.addAttribute("message", "Korisnik je uspesno sacuvan.");*/
 		return "unos/Registracija";
 	}
 	
@@ -29,14 +36,19 @@ public class UserController {
 		return "unos/Registracija.jsp";
 	}
 	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
-
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
-
-        return "login";
+	@RequestMapping(value="login", method=RequestMethod.POST)
+    public String login(Model model, HttpServletRequest request, HttpSession session) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		User user = urr.login(username, password);
+		if(user!=null){
+			request.getSession().setAttribute("messagee", "");
+			request.getSession().setAttribute("user", user);
+			session.setAttribute("userr", user);
+			return "home";
+		}
+		request.getSession().setAttribute("messagee", "Username or password does not exist");
+		return "unos/login";
     }
 }
